47393d2d656ab8bdb3dab2da02f8646885d66f7c
Hannah <hannahjoo24@gmail.com>
2020-07-08 16:57:09 -0700

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify 
+from flask import Blueprint, jsonify, request 
+from . import db  
+from .models import Movie
 
 main = Blueprint('main', __name__)
 
@@ -6,6 +8,13 @@ main = Blueprint('main', __name__)
 @main.route('/add_movie', methods=['POST'])
 
 def add_movie():
+  movie_data = request.get_json()
+
+  # create new movie object
+  new_movie = Movie(title=movie_data['title'], rating=movie_data['rating'])
+
+  db.session.add(new_movie) # add to the database
+  db.session.commit() # commit it
 
   return 'Done', 201  # success
 
@@ -13,8 +22,11 @@ def add_movie():
 @main.route('/movies')
 
 def movies():
-  
+  movie_list = Movie.query.all()
   movies = []
+
+  for movie in movie_list:
+    movies.append({'title': movie.title, 'rating': movie.rating})
 
   return jsonify({'movies' : movies})
 
